a282cb5c9b9e88bd48fb7d5ef6fb5ded18ede85d
GXK2003 <62833006+fuck2003@users.noreply.github.com>
2026-06-02 22:01:23 +0800

    Create stock.py

---
+++ stock.py
@@ -0,0 +1,37 @@
+import requests
+import os
+
+API_KEY = os.environ["FINNHUB_KEY"]
+DINGTALK_WEBHOOK = os.environ["DINGTALK_WEBHOOK"]
+
+WATCH_LIST = {
+    "TSLA": 1
+}
+
+for symbol, target in WATCH_LIST.items():
+
+    url = f"https://finnhub.io/api/v1/quote?symbol={symbol}&token={API_KEY}"
+
+    data = requests.get(url).json()
+
+    price = data.get("c", 0)
+
+    print(symbol, price)
+
+    if price > target:
+
+        msg = {
+            "msgtype": "text",
+            "text": {
+                "content":
+                f"【K股票预警】\n"
+                f"股票：{symbol}\n"
+                f"当前价格：{price}\n"
+                f"触发条件：>{target}"
+            }
+        }
+
+        requests.post(
+            DINGTALK_WEBHOOK,
+            json=msg
+        )
